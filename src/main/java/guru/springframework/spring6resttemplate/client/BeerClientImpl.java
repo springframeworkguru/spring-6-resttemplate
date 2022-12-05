4dec0fd3ec91a30044ada7924e4716c9f861cc7b
John Thompson <john@springframework.guru>
2022-12-05 10:24:27 -0500

    adding get as map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 /**
  * Created by jt, Spring Framework Guru.
  */
@@ -17,12 +19,18 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , String.class);
+
+        ResponseEntity<Map> mapResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
 
         System.out.println(stringResponse.getBody());
 

562dbc36a3f75b254509fe90d509648f98449409
John Thompson <john@springframework.guru>
2022-12-10 10:55:59 -0500

    adding uri components builder

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -24,8 +25,11 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
+
+
         ResponseEntity<BeerDTOPageImpl> response =
-                restTemplate.getForEntity(GET_BEER_PATH , BeerDTOPageImpl.class);
+                restTemplate.getForEntity(uriComponentsBuilder.toUriString() , BeerDTOPageImpl.class);
 
 
         return response.getBody();

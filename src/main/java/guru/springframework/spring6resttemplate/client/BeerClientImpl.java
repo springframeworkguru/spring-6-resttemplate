1b9b9695121cc899e21a538779e91d8c94a9cd8f
John Thompson <john@springframework.guru>
2022-12-10 13:25:18 -0500

    adding http POST

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 /**
@@ -29,9 +30,8 @@ public class BeerClientImpl implements BeerClient {
     public BeerDTO createBeer(BeerDTO newDto) {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<BeerDTO> response = restTemplate.postForEntity(GET_BEER_PATH, newDto, BeerDTO.class);
-
-        return null;
+        URI uri = restTemplate.postForLocation(GET_BEER_PATH, newDto);
+        return restTemplate.getForObject(uri.getPath(), BeerDTO.class);
     }
 
     @Override

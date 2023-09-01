8b2897bc38857d1d63eb49201ba2cfa8989611af
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-01 09:36:13 +0300

    Simple GET request with Spring RestTemplate

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,32 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private final static String API_URL = "http://localhost:8080/api/v1/beer";
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+
+
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(API_URL, String.class);
+
+        System.out.println(stringResponse.getBody());
+
         return null;
     }
 }

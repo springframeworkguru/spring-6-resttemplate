87e412852ee5ff82bf78f1461cbdaabd2624c50f
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-01 13:36:00 +0300

    Use assertions to verify API response

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,9 +1,15 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -13,19 +19,30 @@ class BeerClientImplTest {
 
     @Test
     void listBeersNoBeerName() {
-
-        beerClient.listBeers(null, null, null, null, null);
-
+        Page<BeerDTO> result = beerClient.listBeers(null, null, null, null, null);
+        assertThat(result.getContent()).isNotNull();
     }
 
     @Test
-    void listBeers() {
-        beerClient.listBeers("ALE", null, null, null, null);
+    void listBeersByName() {
+        Page<BeerDTO> result = beerClient.listBeers("ALE", null, null, null, null);
+        assertThat(result.getContent()).isNotNull();
     }
 
     @Test
-    void listBeersByBeerStyle() {
-        beerClient.listBeers(null, BeerStyle.ALE, null, null, null);
+    void listBeersByStyle() {
+        Page<BeerDTO> result = beerClient.listBeers(null, BeerStyle.ALE, null, null, null);
+        assertThat(result.getContent()).isNotNull();
+    }
+
+    @Test
+    void listBeersWithDifferentPageSizes() {
+        for (Integer pageSize = 5; pageSize <= 25; pageSize+=5) {
+            Page<BeerDTO> result = beerClient.listBeers(null, null, null, null, pageSize);
+            System.out.println("Testing API with page size of " + pageSize.toString());
+            assertEquals(pageSize, result.getContent().size());
+        }
+        System.out.println("Paging works! :)");
     }
 
 }

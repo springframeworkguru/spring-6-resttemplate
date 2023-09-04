8eb80a693e483763b9c0a743314a0b6e857c10d4
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-04 11:06:35 +0300

    Fixed client tests by using HTTP Basic Auth with RestTemplate

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -12,6 +12,13 @@ public class RestTemplateBuilderConfig {
 
     @Value("${rest.template.rootUrl}")
     String rootUrl;
+
+    @Value("${rest.template.username}")
+    String username;
+
+    @Value("${rest.template.password}")
+    String password;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
 
@@ -21,6 +28,8 @@ public class RestTemplateBuilderConfig {
         DefaultUriBuilderFactory uriBuilderFactory =
                 new DefaultUriBuilderFactory(rootUrl);
 
-        return builder.uriTemplateHandler(uriBuilderFactory);
+        RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username, password);
+
+        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
     }
 }

2a75a00b8fd383aee53460cd2ee7cfc4d319d865
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-04 09:40:08 +0300

    Test update operation (PUT) with mock REST server

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -29,8 +29,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withAccepted;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 @RestClientTest
 @Import(RestTemplateBuilderConfig.class)
@@ -122,15 +121,14 @@ class BeerClientMockTest {
 
     @Test
     void testUpdateBeer() {
-        URI uri = UriComponentsBuilder.fromPath(BeerClientImpl.GET_BEER_BY_ID_PATH).build(dto.getId());
 
-        server.expect(method(HttpMethod.POST))
-                .andExpect(requestTo(URL + BeerClientImpl.GET_BEER_PATH))
-                .andRespond(withAccepted().location(uri));
+        server.expect(method(HttpMethod.PUT))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
+                .andRespond(withNoContent());
 
         mockGetOperation();
 
-        BeerDTO responseDto = beerClient.createBeer(dto);
+        BeerDTO responseDto = beerClient.updateBeer(dto);
         assertThat(responseDto.getId()).isEqualTo(dto.getId());
     }
 }

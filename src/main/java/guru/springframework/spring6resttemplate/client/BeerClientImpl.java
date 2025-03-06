ffd711ce409cecbdcbfb6bd0cfd40e492d11f6df
alinems <5702162+alinems@users.noreply.github.com>
2025-03-06 19:50:37 +0100

    201 get list as map

---
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -0,0 +1,39 @@
+package guru.springframework.spring6resttemplate.client;
+
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@RequiredArgsConstructor
+@Service
+public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+
+    @Override
+    public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , String.class);
+
+        ResponseEntity<Map> mapResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+
+        System.out.println(stringResponse.getBody());
+
+        return null;
+    }
+}
---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
+
+    @Test
+    void listBeers() {
+
+        beerClient.listBeers();
+    }
+}

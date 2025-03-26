5a0901f2a54de5706f68b62033757a6fee9b531e
alinems <5702162+alinems@users.noreply.github.com>
2025-03-26 09:39:54 +0100

    assignment 22

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -1,12 +1,15 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
 public interface BeerClient {
+    Page<BeerDTO> listBeers();
 
-    Page<BeerDTO> listBeers(String beerName);
+    Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
+                            Integer pageSize);
 }

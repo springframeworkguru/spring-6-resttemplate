b1de1f76172588288ffce3c583701e2c6c1f2c67
John Thompson <john@springframework.guru>
2026-01-04 08:00:02 -0500

    refactor packages

--- src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring7resttemplate/client/BeerClientImplTest.java
@@ -1,10 +1,18 @@
 package guru.springframework.spring7resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
+
+        beerClient.listBeers();
     }
 }
